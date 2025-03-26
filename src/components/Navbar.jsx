7605f0e4e56e99b9eff75d80433fdb1cbb2a6fee
rrex971 <75212090+rrex971@users.noreply.github.com>
2025-03-26 16:45:38 +0530

    improve homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     // TODO
     return (
-    <div className="w-full h-48 flex justify-between items-center px-8 py-4 font-bold bg-orange-peel-400 text-4xl text-orange-100">
+    <div className="w-full h-32 flex justify-between items-center px-8 py-4 font-bold bg-orange-peel-400 text-4xl text-orange-100">
         <div>Aspira</div>
         <div></div>
     </div>
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,20 @@
+import { PiStudentFill, PiChalkboardTeacherFill } from "react-icons/pi"
+import { RiAdminFill } from "react-icons/ri";
+
 const Home = () => {
     return (
-        <div className="flex justify-evenly items-center h-screen">
-            <div className="app-title font-bold text-3xl">
-                STUDENT
+    <div className="flex font-main justify-evenly items-center h-screen">
+            <div className="app-title flex flex-col items-center font-bold text-3xl bg-orange-peel-200 p-32 rounded-4xl">
+                <PiStudentFill size={96}/>
+                <div>Student Login</div>
             </div>
-            <div className="app-title font-bold text-3xl">
-                FACULTY
+            <div className="app-title flex flex-col items-center font-bold text-3xl bg-orange-peel-200 p-32 rounded-4xl">
+                <PiChalkboardTeacherFill size={96}/>
+                <div>Faculty Login</div>
             </div>
-            <div className="app-title font-bold text-3xl">
-                ADMIN
+            <div className="app-title flex flex-col items-center font-bold text-3xl bg-orange-peel-200 p-32 rounded-4xl">
+                <RiAdminFill size={96}/>
+                <div>Admin Login</div>
             </div>
         </div>
     )
