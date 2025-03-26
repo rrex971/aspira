c0f43989fa4476007cce54a36cec79cf7024d72a
Jeevikarajan <160390578+Jeevikarajan@users.noreply.github.com>
2025-03-26 16:27:28 +0530

    basicheader

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 const Navbar = () => {
-    // TODO
     return (
-    <>
-    </>
-    )
-}
+        <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
+            {/* Left: ASPIRA */}
+            <p className="text-xl font-bold font-main">ASPIRA</p>
+
+            {/* Right: Login */}
+            <p className="text-sm cursor-pointer hover:underline font-main">Login</p>
+        </div>
+    );
+};
 
 export default Navbar;
