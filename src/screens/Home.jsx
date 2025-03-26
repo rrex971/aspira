dcf86a35efd6feddac08ca68a3b20f5088645001
bhen11 <bhenedix.paul@gmail.com>
2025-03-26 22:49:17 +0530

    homechqnge

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,6 @@
-import { PiStudentFill, PiChalkboardTeacherFill } from "react-icons/pi"
+import { PiStudentFill, PiChalkboardTeacherFill } from "react-icons/pi";
 import { RiAdminFill } from "react-icons/ri";
+import { Link } from "react-router-dom";  // ✅ Import Link
 import LoginButton from "../components/LoginButton";
 
 const Home = () => {
@@ -8,12 +9,18 @@ const Home = () => {
         <div className="font-bold text-3xl md:text-6xl flex justify-center mt-8 md:mt-16">Welcome to Aspira!</div>
         <div className="font-medium text-2xl mx-8 flex justify-center mt-4">A digital platform that provides Students, Faculty and Administrators supporting efficient and easy access and management.</div>
         <div className="flex-col mx-4 flex md:flex-row space-y-8 font-main justify-center md:justify-evenly items-center mb-64 mt-16 h-fit">
-                <LoginButton icon={<PiStudentFill size={96} />} category="Student"/>
+            
+            <LoginButton icon={<PiStudentFill size={96} />} category="Student"/>
+
+            {/* ✅ Wrapped Faculty LoginButton with Link */}
+            <Link to="/faculty">
                 <LoginButton icon={<PiChalkboardTeacherFill size={96}/>} category="Faculty"/>
-                <LoginButton icon={<RiAdminFill size={96}/>} category="Admin"/>
+            </Link>
+
+            <LoginButton icon={<RiAdminFill size={96}/>} category="Admin"/>
         </div>
     </div>
-    )
+    );
 }
 
 export default Home;
