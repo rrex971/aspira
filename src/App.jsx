192decc97b7d2667b4f416f1318b97db91f9a0ec
joshPokala <198428997+joshPokala@users.noreply.github.com>
2025-03-26 22:17:07 +0530

    josh

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,27 @@ import {
   BrowserRouter as Router,
   Routes,
   Route
+ 
 } from "react-router";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./screens/Home";
 import Student from "./screens/Student";
-import Faculty from "./screens/Faculty";
+import Admin from "./screens/Admin";
 import { AnimatePresence } from 'motion/react'
 
 
 const App = () => {
-
+  
   return (
     <>
       <Navbar /> 
       <AnimatePresence>
           <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Home />} />
-            <Route path="/Faculty"element = {<Faculty/>} />
-            <Route path="/student" element={<Student/>}/>
+            <Route path="/student" element={<Student />}/>
+            <Route path="/admin" element={<Admin />}/>
           </Routes>
         </AnimatePresence>
       <Footer />
