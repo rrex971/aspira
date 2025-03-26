b18f478098bddb0bdcac8ec35142bf9e70b9e13c
rrex971 <75212090+rrex971@users.noreply.github.com>
2025-03-26 19:57:45 +0530

    fix homepage

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="flex-col items-center">
         <div className="font-bold text-3xl md:text-6xl flex justify-center mt-8 md:mt-16">Welcome to Aspira!</div>
         <div className="font-medium text-2xl mx-8 flex justify-center mt-4">A digital platform that provides Students, Faculty and Administrators supporting efficient and easy access and management.</div>
-        <div className="flex-col mx-4 md:flex space-y-8 font-main justify-center md:justify-evenly items-center mb-64 mt-16 h-fit">
+        <div className="flex-col mx-4 flex md:flex-row space-y-8 font-main justify-center md:justify-evenly items-center mb-64 mt-16 h-fit">
                 <LoginButton icon={<PiStudentFill size={96} />} category="Student"/>
                 <LoginButton icon={<PiChalkboardTeacherFill size={96}/>} category="Faculty"/>
                 <LoginButton icon={<RiAdminFill size={96}/>} category="Admin"/>
