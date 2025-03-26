34feec074ca29208843f6320458d4b0241a46680
Jeevikarajan <160390578+Jeevikarajan@users.noreply.github.com>
2025-03-26 20:35:50 +0530

    studentattendance

---
+++ src/screens/Student-attendance.jsx
@@ -0,0 +1,11 @@
+const Attendance = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-3xl md:text-6xl flex justify-center mt-8 md:mt-16">
+                Attendance View
+            </h1>
+        </div>
+    );
+};
+
+export default Attendance;
--- src/screens/Student.jsx
+++ src/screens/Student.jsx
@@ -5,7 +5,7 @@ const Student = () => {
         <div>
             <nav>
                 <ul>
-                    <li><Link to="/attendance">Attendance</Link></li>
+                    <li className><Link to="/Student-attendance">Attendance</Link></li>
                     <li><Link to="/grades">Grades</Link></li>
                     <li><Link to="/timetable">Timetable</Link></li>
                     <li><Link to="/courses">Courses</Link></li>
