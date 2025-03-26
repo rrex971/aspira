16566626891aa0701c3514c231745ccab8f4fb2c
Jeevikarajan <160390578+Jeevikarajan@users.noreply.github.com>
2025-03-26 22:27:45 +0530

    adminbasic

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./screens/Home";
 import Student from "./screens/Student";
-<<<<<<< HEAD
 import Admin from "./screens/Admin";
-=======
 import Sattendance from "./screens/Sattendance";
 import Faculty from "./screens/Faculty";
-<<<<<<< HEAD
 import Sgrades from "./screens/Sgrades";
-=======
->>>>>>> 1cc49b6f8fa025f55aad84262f4b1f6c06f3f77a
->>>>>>> d590514d53c743a5394ccdfda3bcf0838aba920c
 import { AnimatePresence } from 'motion/react'
 
 
@@ -30,20 +24,17 @@ const App = () => {
       <AnimatePresence>
           <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Home />} />
-<<<<<<< HEAD
+
             <Route path="/student" element={<Student />}/>
             <Route path="/admin" element={<Admin />}/>
-=======
+
             <Route path="/Faculty"element = {<Faculty/>} />
             <Route path="/student" element={<Student/>}/>
-<<<<<<< HEAD
             <Route path="/sattendance" element={<Sattendance/>}/>
             <Route path="/sgrades" element={<Sgrades/>}/>
-=======
-            <Route path="/student/sattendance" element ={<Sattendance/>}/>
-            <Route path="/student/sgrades" element={<Sgrades/>}/>
->>>>>>> 1cc49b6f8fa025f55aad84262f4b1f6c06f3f77a
->>>>>>> d590514d53c743a5394ccdfda3bcf0838aba920c
+            <Route path="/sattendance" element ={<Sattendance/>}/>
+            <Route path="/sgrades" element={<Sgrades/>}/>
+
           </Routes>
         </AnimatePresence>
       <Footer />
