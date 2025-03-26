d263b9ab280c5f72dc7eeaf479002bc0083d3a92
Jeevikarajan <160390578+Jeevikarajan@users.noreply.github.com>
2025-03-26 16:09:05 +0530

    basic-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 const Footer = () => {
     // TODO
     return (
-    <>
-    </>
+    <footer className="font-main"> 
+        <h6>ABOUT US:</h6>
+        <p> Aspira- a one stop destination for all student problems</p>
+        <h6> CONTACT US: </h6>
+        <p> Email: vitchennai1234@gmail.com</p> 
+        <p>Phone: 9384020998</p> 
+    </footer>
+
     )
 }
 
