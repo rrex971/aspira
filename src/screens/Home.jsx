2f4e1a6e215ad697e580c28d7835450a70a3551c
rrex971 <75212090+rrex971@users.noreply.github.com>
2025-03-26 15:08:09 +0530

    Add homepage

---
+++ src/screens/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <header className="w-full h-48 font-bold text-3xl">
+                Home Page
+            </header>
+        </div>
+    )
+}
