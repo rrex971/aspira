9eae396fa62bbc3985217764e0daa694f28d614e
rrex971 <75212090+rrex971@users.noreply.github.com>
2025-03-26 19:45:01 +0530

    create LoginButton component for reused buttons, animate on hover

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,6 @@
 import { PiStudentFill, PiChalkboardTeacherFill } from "react-icons/pi"
 import { RiAdminFill } from "react-icons/ri";
+import LoginButton from "../components/LoginButton";
 
 const Home = () => {
     return (
@@ -7,18 +8,9 @@ const Home = () => {
         <div className="font-bold text-6xl flex justify-center mt-16">Welcome to Aspira!</div>
         <div className="font-medium text-2xl flex justify-center mt-4">A digital platform that provides Students, Faculty and Administrators supporting efficient and easy access and management.</div>
         <div className="flex font-main justify-evenly items-center mb-64 mt-16 h-fit">
-                <div className="app-title flex flex-col items-center font-bold text-3xl bg-orange-peel-200 p-32 rounded-4xl">
-                    <PiStudentFill size={96}/>
-                    <div>Student Login</div>
-                </div>
-                <div className="app-title flex flex-col items-center font-bold text-3xl bg-orange-peel-200 p-32 rounded-4xl">
-                    <PiChalkboardTeacherFill size={96}/>
-                    <div>Faculty Login</div>
-                </div>
-                <div className="app-title flex flex-col items-center font-bold text-3xl bg-orange-peel-200 p-32 rounded-4xl">
-                    <RiAdminFill size={96}/>
-                    <div>Admin Login</div>
-                </div>
+                <LoginButton icon={<PiStudentFill size="96" />} category="Student"/>
+                <LoginButton icon={<PiChalkboardTeacherFill size={96}/>} category="Faculty"/>
+                <LoginButton icon={<RiAdminFill size={96}/>} category="Admin"/>
         </div>
     </div>
     )
